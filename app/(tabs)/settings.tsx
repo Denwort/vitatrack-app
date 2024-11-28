c61d22769da94c0f18af5907d900f29aa75295e3
David <davidmm1107@hotmail.com>
2024-11-28 17:20:58 -0500

    integrado register y settings

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,11 +1,41 @@
 import MyButton from "@/components/MyButton";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
-import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import { View, Text, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import axios from "axios";
+import apiURL from "../../api/usuariosApi";
+import { StyleSheet } from "react-native";
+
+
+interface UserProfile {
+  nombre: string;
+  correo: string;
+  foto: string;
+}
 
 export default function Settings() {
+  const [userProfile, setUserProfile] = useState<UserProfile | null>(null);
   const router = useRouter();
+
+  const fetchUserProfile = async () => {
+    try {
+      const userId: string | null = await AsyncStorage.getItem("user");
+
+      const response = await axios.post(`${apiURL}/profile/me`, {userId});
+
+      console.log(response.data)
+
+      setUserProfile(response.data); // Asumimos que el API devuelve los datos del perfil del usuario
+    } catch (err) {
+      console.error("Error al obtener el perfil del usuario:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserProfile();
+  }, []);
 
   const handlePress = async () => {
     try {
@@ -18,8 +48,63 @@ export default function Settings() {
   };
 
   return (
-    <SafeAreaView>
-      <MyButton label={"Cerrar sesion"} outlined={true} onPress={handlePress} />
+    <SafeAreaView style={{ flex: 1, padding: 20 }}>
+      {userProfile ? (
+        <>
+          <View style={styles.profileContainer}>
+            {/* Imagen de perfil */}
+            <View style={styles.profileImageContainer}>
+              <Image
+                source={{ uri: userProfile.foto }}
+                style={styles.profileImage}
+              />
+            </View>
+            {/* Nombre y correo */}
+            <Text style={styles.profileName}>{userProfile.nombre}</Text>
+            <Text style={styles.profileEmail}>{userProfile.correo}</Text>
+          
+
+            {/* Botón de cierre de sesión */}
+            <MyButton
+              label={"Cerrar sesión"}
+              outlined={true}
+              onPress={handlePress}
+            />
+
+          </View>
+        </>
+      ) : (
+        <Text>Cargando perfil...</Text> // Muestra un mensaje mientras se carga el perfil
+      )}
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    alignItems: "center",
+    marginBottom: 40,
+    marginTop: 40,
+  },
+  profileImageContainer: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    overflow: "hidden",
+    marginBottom: 20,
+  },
+  profileImage: {
+    width: "100%",
+    height: "100%",
+  },
+  profileName: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  profileEmail: {
+    fontSize: 16,
+    color: "gray",
+    marginBottom: 30,
+  },
+});
--- app/register.tsx
+++ app/register.tsx
@@ -12,35 +12,67 @@ import {
   Image,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import axios from "axios";
+import apiURL from "../api/usuariosApi";
 
 const RegisterScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
   const [name, setName] = useState("");
+  const [code, setCode] = useState("");
+  const [isConfirmed, setIsConfirmed] = useState(false);
   const router = useRouter();
 
-  const handleRegister = () => {
+  const handleRegister = async () => {
     // Lógica de registro aquí
     console.log("Crear cuenta con", name, email, password);
-    router.replace("/login");
+    try {
+      const response = await axios.post(`${apiURL}/auth/register`, { correo: email, contrasena: password, nombre: name });
+      
+      setIsConfirmed(true)
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  };
+
+  const handleConfirm = async () => {
+    // Lógica para confirmar la cuenta con el código recibido
+    try {
+      const response = await axios.post(`${apiURL}/auth/confirm`, {
+        correo: email,
+        codigo: code,
+      });
+      setIsConfirmed(true);
+      router.replace("/login");  // Redirigir al login después de la confirmación
+    } catch (err) {
+      console.error("Error confirmando el código:", err);
+    }
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Create your account</Text>
-      <View style={styles.formContainer}>
-        <MyTextInput label={"Name"} onChangeText={setName} />
-        <MyTextInput label={"Email"} onChangeText={setEmail} />
-        <MyTextInput label={"Password"} onChangeText={setPassword} />
-        <MyTextInput label={"Confirm password"} onChangeText={setPassword2} />
-        <Image source={require("@/assets/images/register-logo.png")} />
-      </View>
-      <MyButton
-        label="Create account"
-        outlined={false}
-        onPress={handleRegister}
-      />
+      {!isConfirmed ? (
+        <>
+          <Text style={styles.title}>Create your account</Text>
+          <View style={styles.formContainer}>
+            <MyTextInput label={"Name"} onChangeText={setName} />
+            <MyTextInput label={"Email"} onChangeText={setEmail} />
+            <MyTextInput label={"Password"} onChangeText={setPassword} />
+            <MyTextInput label={"Confirm password"} onChangeText={setPassword2} />
+            <Image source={require("@/assets/images/register-logo.png")} />
+          </View>
+          <MyButton label="Create account" outlined={false} onPress={handleRegister} />
+        </>
+      ) : (
+        <>
+          <Text style={styles.title}>Enter the confirmation code</Text>
+          <View style={styles.formContainer}>
+            <MyTextInput label={"Confirmation Code"} onChangeText={setCode} />
+          </View>
+          <MyButton label="Confirm account" outlined={false} onPress={handleConfirm} />
+        </>
+      )}
     </SafeAreaView>
   );
 };
